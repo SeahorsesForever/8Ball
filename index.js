e1025fb14c7000eeff523eae172a9c6f7b021276
ruzich12@gmail.com <ruzich12@gmail.com>
2021-12-13 09:12:19 -0700

    modified the index.js file.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let computerChoice = Math.floor(Math.random() * 3);
 
 function clickEvent() {
-    if (computerChoice === 0 ) {
+    if (computerChoice = 0 ) {
         alert("You will perish soon.");
-    } else if (computerChoice === 1) {
+    } else if (computerChoice = 1) {
         alert("Wealth will find you.");
-    } else if (computerChoice === 2) {
+    } else if (computerChoice = 2) {
         alert("Love is in the air.");
     }
 }
