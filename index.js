a0b1487f612e863ab71415c30bb17e9842b750aa
Dan Ruzich <94571522+SeahorsesForever@users.noreply.github.com>
2021-12-13 11:38:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 function clickEvent() {
     let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice = 0 ) {
+    if (computerChoice === 0 ) {
         alert("You will perish soon.");
-    } else if (computerChoice = 1) {
+    } else if (computerChoice === 1) {
         alert("Wealth will find you.");
-    } else if (computerChoice = 2) {
+    } else if (computerChoice === 2) {
         alert("Love is in the air.");
     }
 }
